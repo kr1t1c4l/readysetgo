f41b8fe09d8e59a56b4074b4a05a200d607f005c
Benjamin McFetridge <b.mcfetridge@hotmail.com>
2015-06-04 21:32:05 +1200

    Database functionality for Find A Stop page
    Added:
    
    All stops (geolocation only) of all route #s between 1-100
    All stops geolocations for a given route (id)
    All information for a given stop
    
    database queries

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.sendfile('index.html', {root: __dirname })
 });
+
 // output stops
 app.get('/stops/:id', function (request, response) {
   console.log('Call on stops for id ', request.params.id);
@@ -22,10 +23,57 @@ app.get('/stops/:id', function (request, response) {
     });
   });
 });
-// output route path
+
+// Output geolocation route path
 app.get('/path/:id', function (request, response) {
   console.log('Call on shapes (route path) for id ', request.params.id);
   var sql = "SELECT trips.trip_id, shapes.shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence FROM trips INNER JOIN shapes ON shapes.shape_id = trips.shape_id WHERE trips.trip_id = "+request.params.id+";"
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query(sql,function(err, result) {
+        done();
+        if (err)
+          { console.error(err); response.send("Error " + err); }
+        else
+          { response.send(result.rows); }
+    });
+  });
+});
+
+// All stops (geolocation only) of all route #s between 1-100
+app.get('/allstops', function (request, response) {
+  console.log('Call on stoptimes, stops, trips, routes for route_id 1-100', request.params.id);
+  var sql = "SELECT stops.stop_lat, stops.stop_lon FROM stoptimes INNER JOIN stops ON stoptimes.stop_id = stops.stop_id INNER JOIN trips ON stoptimes.trip_id = trips.trip_id INNER JOIN routes ON trips.route_id = routes.route_id WHERE routes.route_id BEWTEEN  1 AND 100;"
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query(sql,function(err, result) {
+        done();
+        if (err)
+          { console.error(err); response.send("Error " + err); }
+        else
+          { response.send(result.rows); }
+    });
+  });
+});
+
+
+// All stops geolocations for a given route (id)
+app.get('/stopgeo/:id', function (request, response) {
+  console.log('Call on stoptimes, stops, trips, routes for route_id=id', request.params.id);
+  var sql = "SELECT stops.stop_lat, stops.stop_lon FROM stoptimes INNER JOIN stops ON stoptimes.stop_id = stops.stop_id INNER JOIN trips ON stoptimes.trip_id = trips.trip_id INNER JOIN routes ON trips.route_id = routes.route_id WHERE routes.route_id = " +request.params.id+";"
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query(sql,function(err, result) {
+        done();
+        if (err)
+          { console.error(err); response.send("Error " + err); }
+        else
+          { response.send(result.rows); }
+    });
+  });
+});
+
+// All information for a given stop
+app.get('/stopallinfo/:id', function (request, response) {
+  console.log('Call on stops for route_id=id', request.params.id);
+  var sql = "SELECT * FROM stops WHERE routes.route_id = " +request.params.id+";"
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       client.query(sql,function(err, result) {
         done();
