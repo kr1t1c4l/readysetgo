fb588466ed04489ee9ba29abd891e56569684195
Benjamin McFetridge <b.mcfetridge@hotmail.com>
2015-06-11 23:40:16 +1200

    Added cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var cors = require('cors');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(cors());
 
 app.get('/', function(req, res) {
   res.sendfile('index.html', {root: __dirname })
